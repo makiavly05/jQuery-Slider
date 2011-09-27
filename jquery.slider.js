afef78441d4fc9ef58e0ecf79c73c5bf44d486fb
Neo Geek <neogeek0101@gmail.com>
2011-09-26 20:15:07 -0400

    Placed project URL on new line to be constant with other projects.

--- jquery.slider.js
+++ jquery.slider.js
@@ -1,6 +1,7 @@
 /* ------------------------------------------------------------
  
- jQuery Slider version 0.1 (http://neo-geek.net/work/jquery-slider/)
+ jQuery Slider version 0.1
+ http://neo-geek.net/work/jquery-slider/
  
  Copyright (c) 2011 Neo Geek
  Dual-licensed under both MIT and BSD licenses.
